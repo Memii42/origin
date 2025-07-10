89036ab45b727118bf8cdcf29dbe9390b5855a11
SO JUNG LEE <4jeong22@gmail.com>
2025-07-10 21:22:07 +0900

    .

--- src/dt_algorithm_beginner/list/LinkedListWork.java
+++ src/dt_algorithm_beginner/list/LinkedListWork.java
@@ -1,4 +1,47 @@
 package dt_algorithm_beginner.list;
 
 public class LinkedListWork {
+
+    public static class CustomLinkedList{
+        private static class Node{
+            // 객체 == 노드
+            int value;
+            Node pointer;
+
+            public Node(int value) {
+                this.value = value;
+                this.pointer = null;
+            }
+        }
+
+        private Node head; //list의 시작점
+        private int size;
+
+        public CustomLinkedList(){
+            this.size = 0;
+        }
+
+        public void add(int value) {
+            if (head == null) {
+                head = new Node(value);
+                /*STUDY: 내용정리
+                Node 객체 = 값(vlaue) + 연결(pointer)
+
+                 */
+
+            }
+        }
+
+    }
+
+
+    public static void maind(String[] args) {
+
+        CustomLinkedList list = new CustomLinkedList();
+
+        list.add(10);
+    }
+
+
+
 }
