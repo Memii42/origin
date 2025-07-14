07244e7d1c4607acd202ba5ead6add3701818fab
SO JUNG LEE <4jeong22@gmail.com>
2025-07-14 21:18:02 +0900

    .

---
+++ src/dt_algorithm_beginner/list/ListPractice1.java
@@ -0,0 +1,24 @@
+    /* 사용자가 입력한 명령어에 따라 리스트 값을 추가/ 삭제 한다.
+       명령어는 I (마지막에 입력; 양수만 가능), D(마지막 값 삭제), E(종료)로 진행되며,
+       결과는 종료 이후에 가장 마지막 값, 모든 값, 사이즈를 출력한다.
+       비어있다면 -1을 출력한다.
+       단, D는 I의 횟수보다 더 많이 입력될 수 없다.
+       자료구조는 직접만드는 것 보다 활용하는게 도움이 더 될 확률이 높음
+     */
+package dt_algorithm_beginner.list;
+
+import java.util.*;
+import java.io.*;
+
+public class ListPractice1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true){
+
+        }
+    }
+
+
+}
